bfc902ef05db0e2b6f6b1a38164e163ea61cf954
vics <vics@essoft.com.tw>
2025-12-17 10:06:34 +0800

    feat: implement BaseInput component with two-way data binding and update package.json for correct entry points

--- dist/index.js
+++ dist/index.js
@@ -1,15 +1,21 @@
-import { createElementBlock, createElementVNode, openBlock, renderSlot } from "vue";
-var __plugin_vue_export_helper_default = (e, c) => {
-	let l = e.__vccOpts || e;
-	for (let [e, u] of c) l[e] = u;
-	return l;
-}, _sfc_main$1 = {}, _hoisted_1$1 = { type: "button" };
-function _sfc_render$1(c, d) {
-	return openBlock(), createElementBlock("button", _hoisted_1$1, [renderSlot(c.$slots, "default")]);
-}
-var BaseButton_default = /* @__PURE__ */ __plugin_vue_export_helper_default(_sfc_main$1, [["render", _sfc_render$1]]), _sfc_main = {}, _hoisted_1 = { class: "form" };
-function _sfc_render(d, f) {
-	return openBlock(), createElementBlock("div", null, [createElementVNode("form", _hoisted_1, [renderSlot(d.$slots, "default", {}, void 0, !0)])]);
-}
-var BaseForm_default = /* @__PURE__ */ __plugin_vue_export_helper_default(_sfc_main, [["render", _sfc_render], ["__scopeId", "data-v-27b1c853"]]);
-export { BaseButton_default as BaseButton, BaseForm_default as BaseForm };
+import { createElementBlock, createElementVNode, defineComponent, openBlock, ref, toDisplayString, watch } from "vue";
+var _hoisted_1 = ["value", "type"], src_default = { BaseInput: /* @__PURE__ */ defineComponent({
+	__name: "BaseInput",
+	props: {
+		value: { default: "" },
+		type: { default: "text" }
+	},
+	setup(o) {
+		let c = ref(""), l = o, u = (e) => {
+			c.value = e.target.value;
+		};
+		return watch(() => l.value, (e) => {
+			console.log(e), c.value = e || "";
+		}, { immediate: !0 }), (o, s) => (openBlock(), createElementBlock("div", null, [createElementVNode("input", {
+			value: c.value,
+			type: l.type,
+			onInput: u
+		}, null, 40, _hoisted_1), createElementVNode("p", null, toDisplayString(c.value), 1)]));
+	}
+}) };
+export { src_default as default };
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
-// import BaseInput from './components/BaseInput.vue';
-import BaseButton from './components/BaseButton.vue';
+import BaseInput from './components/BaseInput.vue';
+// import BaseButton from './components/BaseButton.vue';
 import BaseForm from './components/BaseForm.vue';
-export {
-  // BaseInput,
-  BaseButton,
-  BaseForm,
+export default {
+  BaseInput
 }
+// export {
+//   BaseInput,
+//   BaseButton,
+//   BaseForm,
+// }
